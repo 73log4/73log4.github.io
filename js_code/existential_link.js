5366eea3995b68c8ce7cfb9ec28ba1ffd27be425
73log4 <82608046+73log4@users.noreply.github.com>
2022-08-28 13:37:46 +0300

    a

--- js_code/existential_link.js
+++
@@ -1,3 +0,0 @@
-function bigImage(index){
-    window.location.href = "file:///C:/Users/Owner/Documents/webdev/web2/existential_ind.html?index=0" + String(index);
-}
--- js_code/jewish_motives_link.js
+++
@@ -1,3 +0,0 @@
-function bigImage(index){
-    window.location.href = "file:///C:/Users/Owner/Documents/webdev/web2/jewish_motives_ind.html?index=0" + String(index);
-}
